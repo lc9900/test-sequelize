8c3cbc885a5fead7f5d7ff621f9bea39e52da0a6
lc9900 <whynot_hm@hotmail.com>
2017-08-23 18:36:02 -0400

    weird stuff with hasMany

--- belongsTo.js
+++ belongsTo.js
@@ -1,25 +1,33 @@
 const Sequelize = require('sequelize');
-
 const dbUrl = process.env.DATABASE_URL || 'postgres://localhost/testdb';
 const db = new Sequelize(dbUrl, {logging: true});
 const utils = require('./utils');
-
-
-
-
 const User = db.define('user', {
-    name: Sequelize.STRING
+    name: {
+        type: Sequelize.STRING,
+        get: function(){
+            // Database will not get 'GetModified', but when you retrieve user.name, you will get "GetMOdified name"
+            return  "GetModified " + this.getDataValue('name');
+        },
+        set: function(name){
+            // Database would now have  user's name as 'name SetModified'
+            this.setDataValue('name', name + " SetModified" );
+        }
+    }
 })
 
 const Project = db.define('project', {
-    name: Sequelize.STRING
+    name: Sequelize.STRING,
 })
-
 
 // BelongsTo
 // Many user to one project
 User.belongsTo(Project);
-var user1, project1;
+
+User.belongsTo(User, {as: 'manager'}); // Creates managerId in the user table
+User.hasMany(User, {as: 'worker', foreignKey: 'managerId'}); // No table change, since we defined a foreignKey
+
+var user1, project1, worker1, worker2;
 
 db.sync({force: true})
     .then(() => {
@@ -35,13 +43,28 @@ db.sync({force: true})
         console.log(`project.setUsers ${project.setUsers}`); // Undefined
         console.log(`project.setUser ${project.setUser}`); // Undefined
 
-        // project.name = 'Project X';
-        user.name = 'User X';
-        // return user.setProject(project);
+        console.log(`project.addUser ${project.addUser}`); // Undefined
+        console.log(`project.addUsers ${project.addUsers}`); // Undefined
+
+        // For the self-association
+        console.log(`user.getManager ${user.getManager}`); // defined
+        console.log(`user.getManagers ${user.getManagers}`); // Undefined
+        console.log(`user.setManager ${user.setManager}`); // defined
+        console.log(`user.setManagers ${user.setManagers}`); // Undefined
+
+        console.log(`user.getWorker ${user.getWorker}`); // defined
+        console.log(`user.getWorkers ${user.getWorkers}`); // Undefined
+        console.log(`user.setWorker ${user.setWorker}`); // defined
+        console.log(`user.setWorkers ${user.setWorkers}`); // Undefined
+
+        user.name = 'UserX';
         return user.save()
 
     }).then((user) => {
         user1 = user;
+
+        utils.inform(user1.name);
+
         var project = Project.build({})
         project.name = "Project 1";
         // return user1.setProject(project1); // No project ID inserted
@@ -49,7 +72,35 @@ db.sync({force: true})
 
     }).then(project => {
         return user1.setProject(project); // Now works
-    }).catch(err => {
+    }).then(user => {
+        utils.inform(user.name); // user1.setProject(project) returns user1 sequelize object
+        return user.getProject();
+    }).then(project => {
+        utils.inform(project.name); // user.getProject returns a sequelize object for project
+    }).then(() => {
+        return User.create({
+            name: "Worker1"
+        })
+    }).then(user => {
+        worker1 = user;
+        return User.create({
+            name: 'Worker2'
+        })
+    }).then(user => {
+        worker2 = user;
+
+        return worker2.setManager(user1); // Sets worker2's managerId to user1
+    }).then(() => {
+        console.log('=======================================')
+        return user1.setWorker(worker1);
+    }).then(() => {
+        console.log('=======================================')
+        return user1.setWorker(worker2);
+    }).then(() => {
+        console.log('=======================================')
+        return worker1.setManager(user1);
+    })
+    .catch(err => {
         throw err;
     })
 
